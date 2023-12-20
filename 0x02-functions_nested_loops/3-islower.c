395e5c460f83c700656e23e47cdce28a294c4708
Janicemwendwa <janicemutuma001@gmail.com>
2023-12-19 16:11:54 -0800

    checks for lowercase character

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * islower - checks for lowercase character
+ * Return: 1 for lower case and 0 for anything else
+ * @c - character being checked
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+	return (1);
+	}
+	return (0);
+}
+
