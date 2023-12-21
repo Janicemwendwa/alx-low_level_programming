f507cb8cacfe1a2abd16c846bc8f2525b41ad1de
Janicemwendwa <janicemutuma001@gmail.com>
2023-12-20 18:44:00 -0800

    prints the sum of the even-valued terms

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,7 +3,7 @@
  * main - prints the sum of the even-valued terms
  * Return: nothing
  */
- int main(void)
+int main(void)
 {
 	int i = 0;
 	long j = 1, k = 2, sum = k;
@@ -13,7 +13,7 @@
 	k += j;
 	if (k % 2 == 0)
 	sum += k;
-	j = k -j;
+	j = k - j;
 	++i;
 	}
 	printf("%ld\n", sum);
