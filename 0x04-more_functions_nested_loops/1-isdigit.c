cfeca78bc36b91d73508d3925c40053a19b7a5db
Janicemwendwa <janicemutuma001@gmail.com>
2023-12-21 03:51:50 -0800

    checks if digit is 0 to 9

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit - checks for a digit 0 through 9
+ * @c: digit being checked
+ * Return: 1 if digit 0 if anything else
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
