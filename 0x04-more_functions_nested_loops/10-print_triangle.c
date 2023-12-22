0ca4a27eb7de030034948e15015fae14602fc0a2
Janicemwendwa <janicemutuma001@gmail.com>
2023-12-21 17:37:15 -0800

    prints a triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,25 +6,26 @@
  */
 void print_triangle(int size)
 {
-	int x, y, z;
+	int x, y;
 
-	if (size <= 0)
+	if (size > 0)
 	{
+	for (x = 1; x <= size; x++)
+	{
+	for (y = size - x; y > 0; y--)
+	{
+	_putchar(' ');
+	}
+	for (y = 0; y < x; y++)
+	{
+	_putchar('#');
+	}
+	if (x == size)
+	{
+	continue;
+	}
 	_putchar('\n');
 	}
-	else
-	{
-	for (x = 0; x < size; x++)
-	{
-	for (y = size - x; y > 1; y++)
-	{
-	_putchar(32);
-	}
-	for (z = 0; z <= x; z++)
-	{
-	_putchar(35);
 	}
 	_putchar('\n');
-	}
-	}
 }
