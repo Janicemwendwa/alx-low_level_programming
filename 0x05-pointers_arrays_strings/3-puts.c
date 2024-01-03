824277da043b395a58f956f5e5831bc9422bd617
Janicemwendwa <janicemutuma001@gmail.com>
2024-01-02 16:39:44 -0800

    function that prints a string, in reverse

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,16 +1,13 @@
 #include "main.h"
 /**
- * _puts -  prints a string, followed by a new line, to stdout
+ * _puts - prints a string, followed by a new line, to stdout
  * @str: string
  * Return: nothing
  */
 void _puts(char *str)
 {
 	while (*str)
-	{
-		_putchar(*str++);
-	}
-
+	_putchar(*str++);
 	_putchar('\n');
 }
 	
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_rev - prints a string, in reverse
+ * @s: argument
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+	int i, j, k;
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	k = i;
+	for (j = k - i; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+	_putchar('\n');
+}
+
